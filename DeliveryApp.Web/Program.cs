9aa1f16f75a66f22360cc76a9eb3123f0a67e483
Ionut Nitescu <nitescucornelionut19@gmail.com>
2023-03-01 14:45:13 +0200

    add multiple origins

--- DeliveryApp.Web/Program.cs
+++ DeliveryApp.Web/Program.cs
@@ -38,7 +38,11 @@ app.UseRouting();
 app.UseDefaultFiles();
 app.UseStaticFiles();
 
-app.UseCors(opt => { opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:3000"); });
+app.UseCors(opt =>
+{
+    opt.AllowAnyHeader().AllowAnyMethod().AllowCredentials().WithOrigins("http://localhost:4000",
+        "http://localhost:4001", "http://localhost:4002");
+});
 
 app.UseAuthentication();
 app.UseAuthorization();
